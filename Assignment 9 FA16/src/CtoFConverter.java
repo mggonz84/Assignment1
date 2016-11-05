161b4c70ad369054eb58a2cea1d4372fbf7d6967
mggonz84 <mgonzalez84@mail.stmarytx.edu>
2016-11-05 00:05:48 -0500

    added action for CtoFConvertion and TempConverter

--- Assignment 9 FA16/src/CtoFConverter.java
+++ Assignment 9 FA16/src/CtoFConverter.java
@@ -1,9 +1,16 @@
 import java.awt.*;
-import java.util.Scanner;
-
+import java.awt.event.*;
 import javax.swing.*;
 
 public class CtoFConverter extends JFrame{
+	
+	//final
+	final JLabel c;
+	final JLabel f;
+	final JTextField ctemp;
+	final JTextField ftemp;
+	final JButton convert;
+	
 	public CtoFConverter(){
 		setTitle("Temperature Converter");
 		setLayout(new FlowLayout());// can use new FlowLayout()
@@ -12,45 +19,58 @@ public class CtoFConverter extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//Celsius to Fahrenheit
-		
-		//JPanel panel = new JPanel();
-		
+				
 		//Celsius
-		JLabel c = new JLabel("Celsius: ");
+		c = new JLabel("Celsius: ");
 		c.setSize(200,80);
 		add(c);
 		
-		JTextField ctemp = new JTextField("",5);
-		//int cnum = Integer.parseInt(ctemp.getText());
+		ctemp = new JTextField(5);
 		add(ctemp);
 
 		//Fahrenheit
-		JLabel f = new JLabel("Fahrenheit: ");
+		f = new JLabel("Fahrenheit: ");
 		f.setSize(200,80);
 		add(f);
 		
-		//how to calculate
-		//int fnum = 9 / 5 * cnum + 32;
-		
-		//how to show ftemp
-		JTextField ftemp = new JTextField("",5);	
+		//show ftemp
+		ftemp = new JTextField(5);	
 		add(ftemp);
 		
-		JButton convert = new JButton("Convert");
+		convert = new JButton("Convert");
 		convert.setSize(200,80);
+		convert.addActionListener(new RadioButtonListener());
 		add(convert);
 		
 		
 		setVisible(true);
 		
 	}
+	
+	private class RadioButtonListener implements ActionListener
+	   {
+	      public void actionPerformed(ActionEvent e)
+	      {
+	         String input;          // To hold input
+	         double result = 0.0;   // To hold the conversion
+
+	         // Get input from the text field.
+	         input = ctemp.getText();
+	         
+	       //performs the conversion when button is pressed
+	         
+	        	//convert c to f
+	     		//F = (9/5) C + 32
+	            result = Double.parseDouble(input) * 9 / 5 + 32;
+
+	         // Display the converted distance.
+	         String convertion = Double.toString(result);
+	         ftemp.setText(convertion);	//conversion
+	         
+	      }
+	   }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Scanner user = new Scanner(System.in);
-		
-		//convert c to f
-		//F = (9/5) C + 32
 		
 		new CtoFConverter();
 		
--- Assignment 9 FA16/src/TempConverter.java
+++ Assignment 9 FA16/src/TempConverter.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class TempConverter extends JFrame{
+	
+	//final
+	final JLabel label;
+	final JTextField value;	//inserted value
+	final JTextField cvalue;	//conversion
+	final JRadioButton f;
+	final JRadioButton c;
+	
 	public TempConverter(){
 		setTitle("Temperature Converter");
 		setLayout(new FlowLayout());// can use new FlowLayout()
@@ -9,24 +18,58 @@ public class TempConverter extends JFrame{
 		setSize(700,100);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel label = new JLabel("Enter a value in Celsius or Fahrenheit");
+		//labels, text fields, and radio buttons
+		label = new JLabel("Enter a value in Celsius or Fahrenheit");
 		label.setSize(200,80);
+		
+		value = new JTextField(5);	//inserted value
+		cvalue = new JTextField(5);	//conversion
+
+		f = new JRadioButton("Convert to fahrenheit");
+		c = new JRadioButton("Convert to celsius");
+		
+		//action
+		f.addActionListener(new RadioButtonListener());
+	    c.addActionListener(new RadioButtonListener());
+		
+		//add to frame
 		add(label);
-		
-		JTextField value = new JTextField("", 5);
 		add(value);
-		
-		JRadioButton f = new JRadioButton("Convert to fahrenheit");
 		add(f);
-		
-		JRadioButton c = new JRadioButton("Convert to celsius");
 		add(c);
-		
-		JTextField cvalue = new JTextField("", 5);
 		add(cvalue);
 		
 		setVisible(true);
 	}
+	
+	private class RadioButtonListener implements ActionListener
+	   {
+	      public void actionPerformed(ActionEvent e)
+	      {
+	         String input;          // To hold input
+	         double result = 0.0;   // To hold the conversion
+
+	         // Get input from the text field.
+	         input = value.getText();
+	         
+	         // Determine the button that was clicked and
+	         // perform the selected conversion.
+	         if (e.getSource() == f)
+	         {
+	        	 //c to f
+	            result = Double.parseDouble(input) * 9 / 5 + 32;
+	         }
+	         else if (e.getSource() == c)
+	         {
+	            result = (Double.parseDouble(input) - 32) * 5 / 9;
+	         }
+
+	         // Display the converted distance.
+	         String convertion = Double.toString(result);
+	         cvalue.setText(convertion);	//conversion
+	         
+	      }
+	   }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
